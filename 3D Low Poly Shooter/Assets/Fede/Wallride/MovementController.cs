521e3ea123928145c78fd014df370158532ffd4a
Lennox <57277825+Federico-Bolzan@users.noreply.github.com>
2023-05-22 20:07:25 -0300

    derrape

--- 3D Low Poly Shooter/Assets/Fede/Wallride/MovementController.cs
+++ 3D Low Poly Shooter/Assets/Fede/Wallride/MovementController.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     private CharacterController controller;
-    
+
     public Transform groundCheck;
-    
+
     public LayerMask groundMask;
     public LayerMask wallMask;
-    
+
     private Vector3 input;
     private Vector3 move;
     private Vector3 Yvelocity;
@@ -18,7 +18,7 @@ public class MovementController : MonoBehaviour
     private float speed;
     public float runSpeed;
     public float airSpeed;
-    
+
     private bool isSprinting;
     public float sprintSpeed;
 
@@ -27,17 +27,23 @@ public class MovementController : MonoBehaviour
 
     private float gravity;
     public float normalGravity;
-    
+
     private float startHeight;
     private float crouchHeight = 0.5f;
     private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
     private Vector3 standingCenter = new Vector3(0, 0, 0);
-    
+
     private bool isGrounded;
     private int jumpChanges;
     public float jumpHeight;
-    
-    private bool isWallRunnig, hasWallRun= false;
+
+    private bool isSliding;
+    private float slideTimer;
+    public float maxSlideTimer;
+    public float slideSpeedIncrease;
+    public float slideSpeedDecreased;
+
+    private bool isWallRunning, hasWallRun= false;
     private float wallRunGravity, wallRunSpeedIncrease, wallRunSpeedDecrease;
     private bool onLeftWall, onRightWall;
     private RaycastHit leftWallHit, rightWallHit;
@@ -54,7 +60,7 @@ public class MovementController : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         startHeight = transform.localScale.y;
-        normalFov = playerCamera.fieldOfView;
+        normalFov = playerCamera.fieldOfView; // toma el fov de base como el normal
     }
 
     void Update()
@@ -62,55 +68,58 @@ public class MovementController : MonoBehaviour
         HandleInput();
         CheckWallRun();
         
-        if (isGrounded)
+        if (isGrounded && !isSliding)
         {
-            GroundedMovment();
+            GroundedMovement();
         }
-        else if (!isGrounded && !isWallRunnig)
+        else if (!isGrounded && !isWallRunning)
         {
             AirMovement();
         }
-        else if (isWallRunnig)
+        else if (isWallRunning)
         {
             WallRunMovement();
             DecreaseSpeed(wallRunSpeedDecrease);
         }
+        else if (isSliding)
+        {
+            SlideMovement();
+            DecreaseSpeed(slideSpeedDecreased);
+            slideTimer -= 1f * Time.deltaTime;
+            if (slideTimer <= 0)
+            {
+                isSliding = false;
+            }
+            
+        }
         
-        CheckGround();
         controller.Move(move * Time.deltaTime);
         ApplyGravity();
         CameraEffects();
     }
+    
+    void FixedUpdate()
+    {
+        CheckGround();
+    }
 
     void CameraEffects()
     {
-        float fov;
-
-        if (isWallRunnig==true)
-        {
-            fov = specialFov;
-        }
-        else
-        {
-            fov = normalFov;
-        }
-            
+        float fov = isWallRunning ? specialFov : isSliding ? specialFov : normalFov;
         playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, fov, cameraChangeTime * Time.deltaTime);
 
-        if (isWallRunnig)
+        if (isWallRunning)
         {
             if (onRightWall)
             {
                 tilt = Mathf.Lerp(tilt, wallRunTilt, cameraChangeTime * Time.deltaTime);
             }
-
-            if (onLeftWall)
+            else if (onLeftWall)
             {
                 tilt = Mathf.Lerp(tilt, -wallRunTilt, cameraChangeTime * Time.deltaTime);
             }
         }
-
-        if (!isWallRunnig)
+        else
         {
             tilt = Mathf.Lerp(tilt, 0f, cameraChangeTime * Time.deltaTime);
         }
@@ -124,7 +133,7 @@ public class MovementController : MonoBehaviour
         input = transform.TransformDirection(input);
         input = Vector3.ClampMagnitude(input, 1f);
 
-        if (Input.GetKeyUp(KeyCode.Space) && jumpChanges > 0)
+        if (Input.GetKeyDown(KeyCode.Space) && jumpChanges > 0)
         {
             Jump();
             jumpChanges = 0;
@@ -149,7 +158,7 @@ public class MovementController : MonoBehaviour
         }
     }
 
-    void GroundedMovment()
+    void GroundedMovement()
     {
         speed = isSprinting ? sprintSpeed : isCrouching ? crouchSpeed : runSpeed;
         
@@ -182,13 +191,20 @@ public class MovementController : MonoBehaviour
         move = Vector3.ClampMagnitude(move, speed);
     }
 
+    void SlideMovement()
+    {
+        move += fowardDirection;
+        move = Vector3.ClampMagnitude(move, speed);
+
+    }
+
     void Jump()
     {
-        if (!isGrounded && !isWallRunnig)
+        if (!isGrounded && !isWallRunning)
         {
             jumpChanges -= 1;
         }
-        else if(isWallRunnig)
+        else if(isWallRunning)
         {
             ExitWallRun();
             IncreaseSpeed(wallRunSpeedIncrease);
@@ -201,6 +217,16 @@ public class MovementController : MonoBehaviour
         controller.height = crouchHeight;
         controller.center = crouchingCenter;
         transform.localScale = new Vector3(transform.localScale.x, crouchHeight, transform.localScale.z);
+        if (speed > runSpeed)
+        {
+            isSliding = true;
+            fowardDirection = transform.forward;
+            if (isGrounded)
+            {
+                IncreaseSpeed(slideSpeedIncrease);
+            }
+            slideTimer = maxSlideTimer;
+        }
         isCrouching = true;
     }
 
@@ -210,7 +236,7 @@ public class MovementController : MonoBehaviour
         controller.center = standingCenter;
         transform.localScale = new Vector3(transform.localScale.x, startHeight, transform.localScale.z);
         isCrouching = false;
-        
+        isSliding = false;
     }
     void CheckGround()
     {
@@ -233,7 +259,7 @@ public class MovementController : MonoBehaviour
     
     void ApplyGravity()
     {
-        gravity = isWallRunnig ? wallRunGravity : normalGravity;
+        gravity = isWallRunning ? wallRunGravity : normalGravity;
         Yvelocity.y += gravity * Time.deltaTime;
         controller.Move(Yvelocity * Time.deltaTime);
     }
@@ -245,12 +271,12 @@ public class MovementController : MonoBehaviour
         onLeftWall = Physics.Raycast(transform.position, -transform.right, out leftWallHit, 0.7f, wallMask);
         onRightWall = Physics.Raycast(transform.position, transform.right, out rightWallHit, 0.7f, wallMask);
 
-        if ((onRightWall || onLeftWall)&& !isWallRunnig)
+        if ((onRightWall || onLeftWall)&& !isWallRunning)
         {
             TestWallRun();
         }
 
-        if ((!onRightWall || !onLeftWall)&& isWallRunnig)
+        if ((!onRightWall || !onLeftWall)&& isWallRunning)
         {
             ExitWallRun();
         }
@@ -258,11 +284,11 @@ public class MovementController : MonoBehaviour
 
     void TestWallRun()
     {
-        wallNormal = onLeftWall ? leftWallHit.normal : rightWallHit.normal;
+        wallNormal = onRightWall ? rightWallHit.normal : leftWallHit.normal;
         if (hasWallRun)
         {
             float wallAngle = Vector3.Angle(wallNormal, lastWallNormal);
-            if (wallAngle >15)
+            if (wallAngle > 15)
             {
                 WallRun();
             }
@@ -275,7 +301,7 @@ public class MovementController : MonoBehaviour
     }
     void WallRun()
     {
-        isWallRunnig = true;
+        isWallRunning = true;
         jumpChanges = 1;
         IncreaseSpeed(wallRunSpeedIncrease);
         Yvelocity = new Vector3(0f, 0f, 0f);
@@ -308,7 +334,7 @@ public class MovementController : MonoBehaviour
     }   
     void ExitWallRun()
     {
-        isWallRunnig = false;
+        isWallRunning = false;
         lastWallNormal = wallNormal;
     }
     
