7298ef54f5d0bf2a585d22109cc1bbe33d8b31ed
aguevabb <andresguevara4699@gmail.com>
2023-06-01 18:35:56 -0300

    Menu de pausa

--- 3D Low Poly Shooter/Assets/Andy/Scripts/MenuPausa/MenuPausa.cs
+++ 3D Low Poly Shooter/Assets/Andy/Scripts/MenuPausa/MenuPausa.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuPausa : MonoBehaviour
 {
     public static bool JuegoPausado = false;
-    public GameObject menuPausaUI;
+    public GameObject Panel;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (JuegoPausado == true)
             {
-                Resumir();
+                Resumir(); 
             }
             else
             {
@@ -23,23 +24,28 @@ public class MenuPausa : MonoBehaviour
     }
     public void Resumir()
     {
-        menuPausaUI.SetActive(false);
+        Panel.SetActive(false); 
         Time.timeScale = 1f;
         JuegoPausado = false;
+        Debug.Log("Resumir");
     }
-    void Pausar()
+    public void Pausar()
     {
-        menuPausaUI.SetActive(true);
+        Panel.SetActive(true);
         Time.timeScale = 0f;
         JuegoPausado = true;
+        Debug.Log("Pausar");
     }
     public void CargarMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Menu");
+        Debug.Log("Menu");
     }
     public void Salir()
     {
         Application.Quit();
+        Debug.Log("Salir");
+
     }
 }
