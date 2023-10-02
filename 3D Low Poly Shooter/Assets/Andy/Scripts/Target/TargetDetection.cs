1434e552d59adcad6f45105fff2156ec370323e8
aguevabb <andresguevara4699@gmail.com>
2023-10-02 19:36:25 -0300

    Puzzle Andy 1.0
    Copie la escena del puzzle del lea y le agregue los targts y el script de deteccion de impacto.

---
+++ 3D Low Poly Shooter/Assets/Andy/Scripts/Target/TargetDetection.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            gameObject.tag = "Finish";
+            Debug.Log("Triggered Target by Bullet");
+            Destroy(gameObject);
+        }
+    }
+}
