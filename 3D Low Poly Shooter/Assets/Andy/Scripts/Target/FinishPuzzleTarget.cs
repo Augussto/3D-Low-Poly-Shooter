d586300fb6897ebb7599e65868b55be5ef1408e5
aguevabb <andresguevara4699@gmail.com>
2023-10-19 10:13:31 -0300

    Puzzle Andy terminado

--- 3D Low Poly Shooter/Assets/Andy/Scripts/Target/FinishPuzzleTarget.cs
+++ 3D Low Poly Shooter/Assets/Andy/Scripts/Target/FinishPuzzleTarget.cs
@@ -1,12 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishPuzzleTarget : MonoBehaviour
 {
+		// Use this for initialization
+		int targetsLeft = 0;
+		bool destroyAllTargets = false;
+		void Start()
+		{
+		targetsLeft = 11; // or whatever;
 
-    void Update()
-    {
-        
-    }
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			GameObject[] enemies = GameObject.FindGameObjectsWithTag("Target");
+		targetsLeft = enemies.Length;
+			if (Input.GetKeyDown(KeyCode.W))
+			{
+			targetsLeft--;
+			}
+			if (targetsLeft == 0)
+			{
+				endLevel();
+			}
+		}
+
+		void endLevel()
+		{
+		destroyAllTargets = true;
+		Debug.Log("Level Finished");
+		SceneManager.LoadScene("Escena Augusto Test");
+	}
 }
--- 3D Low Poly Shooter/Assets/Andy/Scripts/Target/TargetDetection.cs
+++ 3D Low Poly Shooter/Assets/Andy/Scripts/Target/TargetDetection.cs
@@ -7,7 +7,7 @@ public class TargetDetection : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Bullet")
+        if (other.tag == "Bullet")
         {
             gameObject.tag = "Finish";
             Debug.Log("Triggered Target by Bullet");
